b9a1e5a6a990f9e054390e6fbc8a4bc8f5a37bb5
PKAditya <pkngssaditya99@gmail.com>
2024-11-28 16:50:04 +0800

    Added log messages to the main scripts.
    Signed-off-by: PKAditya <pkngssaditya99@gmail.com>

--- run.sh
+++ run.sh
@@ -4,6 +4,23 @@
 pip install pyfiglet &> /dev/null
 python3 -m pyfiglet "LKP TESTS"
 
+# Helpers for logs
+mkdir /usr/lib/automation-logs &> /dev/null
+log=/usr/lib/automation-logs/log
+rm -rf $log &> /dev/null
+touch $log
+
+log () {
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> $log
+}
+
+handle_error() {
+        log "Error: $1"
+        echo "Script failed, Check out the logs in /usr/lib/automation-logs for finding about the error"
+        exit
+}
+
+
 # capture current working directory
 loc=$(pwd)
 
@@ -14,43 +31,33 @@ user=$(echo $USER)
 echo "////////////--  STARTING WITH LKP RUNNING  --\\\\\\\\\\\\"
 echo "Distro found: $distro"
 echo "Current user: $user"
+log "Captured distro: $distro and current user: $user"
 echo " "
-
-# creating automation logs
-
-mkdir /usr/lib/automation-logs &> /dev/null
-log=/usr/lib/automation-logs/log
-rm -rf $log &> /dev/null
-touch $log
-
-log () {
-	echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> $log
-}
-
-handle_error() {
-	log "Error: $1"
-	echo "Script failed, Check out the logs in /usr/lib/automation-logs for finding about the error"
-	exit 	
-}
 
 # user input
 read -p "Enter kernel repository path: " KERNEL_DIR
-
+log "Captured user input, KERNEL_DIR: $KERNEL_DIR"
 
 # Modifying sudoers 
 echo 'Amd$1234!' | sudo -S $loc/sudoers.sh $user || handle_error "Couldn't run sudoers modification script"
-
+log "Modified sudoers"
 echo ""
+
+
 if [ "$distro" == "ubuntu" ]; then
   if [ "$user" == "amd" ]; then
+	  log "Entered directory ubuntu"
           echo 'Amd$1234!' | sudo -S $loc/ubuntu/kernel/run.sh $loc $KERNEL_DIR
   else
+	  log "Entered directory ubuntu"
           sudo $loc/ubuntu/kernel/run.sh $loc $KERNEL_DIR
   fi
 else
   if [ "$user" == "amd" ]; then
+	  log "Entered directory centos"
           echo 'Amd$1234!' |  sudo -S $loc/centos/run.sh $user
   else
+	  log "Entered directory centos"
           sudo $loc/centos/run.sh $user
   fi
 fi
--- sudoers.sh
+++ sudoers.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
+# Helpers for logs
+log=/usr/lib/automation-logs/log
+log () {
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> $log
+}
+
+handle_error() {
+        log "Error: $1"
+        echo "Script failed, Check out the logs in /usr/lib/automation-logs for finding about the error"
+        exit
+}
+
 # giving the current user root priviledges
 user=$1
-# echo "$user     ALL=(ALL)       ALL" >> /etc/sudoers
 
 sudoers_pattern="^$user[[:space:]]\+ALL=(ALL)[[:space:]]\+ALL"
 
 if grep -q "$sudoers_pattern" /etc/sudoers; then
     echo "User already has root privileges"
+    log "Aborting modification of sudoers as the user has the privledges"
 else
     # Create a backup
     cp /etc/sudoers /etc/sudoers.bak
-    
+    log "Created a backup file before modifying the sudoers file.
+ 
     # Create temporary file
     TEMP_SUDOERS=$(mktemp)
     
@@ -25,11 +38,13 @@ else
         echo "Successfully added user to sudoers"
     else
         echo "Error: Syntax check failed. Changes not applied."
+	handle_error "Syntax check failed. Changes not applied for user privledges"
         echo "Original sudoers file is unchanged"
     fi
     
     # Clean up
     rm -f "$TEMP_SUDOERS"
+    log "Successfully changed current user privledges to root privledges"
 fi
 
 # Create a backup of sudoers file
@@ -39,6 +54,7 @@ cp /etc/sudoers /etc/sudoers.bak
 #if grep -q "^Defaults.*secure_path.*\/usr\/local\/bin" /etc/sudoers; then
 if grep -q "^Defaults.*secure_path.*/usr/local/bin" /etc/sudoers; then
     echo "/usr/local/bin is already in secure_path"
+    log "Aborting addition of secure_path in defaults, as the path already exists."
     exit 0
 fi
 
@@ -80,6 +96,7 @@ if visudo -c -f "$TEMP_SUDOERS"; then
     echo "Successfully added /usr/local/bin to secure_path"
 else
     echo "Error: Syntax check failed. Changes not applied."
+    handle_error "Syntax check failed. Addition of secure_path to defaults not done"
     echo "Original sudoers file is unchanged"
     rm -f "$TEMP_SUDOERS"
     exit 1
@@ -87,3 +104,4 @@ fi
 
 # Clean up
 rm -f "$TEMP_SUDOERS"
+log "Successfully added path to secure_path in defaults"
