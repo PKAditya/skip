1e5387a5062e61292105145f03456f0581d4e292
PKAditya <pkngssaditya99@gmail.com>
2024-11-28 13:15:59 +0800

    Added sudoers.sh to add the installation destination of lkp
    Signed-off-by: PKAditya <pkngssaditya99@gmail.com>

---
+++ ubuntu/kernel/dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# update the system
+sudo apt update -y
+
+# upgrade the system
+sudo apt upgrade -y
+
+# installing dependencies
+sudo apt install dwarves -y
+sudo apt install rsync -y
---
+++ ubuntu/kernel/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+loc=$1
+
+mkdir /usr/lib/reboot_tmp &> /dev/null
+log=/usr/lib/reboot_tmp/log
+rm $log &> /dev/null
+touch $log
+
+# function defined to log the every step
+log() {
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> $log
+}
+
+# function defined to handle error
+handle_error() {
+	log "ERROR: $1" >> $log
+	exit 1
+}
+
+# user input for kernel directory
+read -p "Enter the Kernel repository path: " KERNEL_DIR
+
+
+$loc/ubuntu/kernel/dependencies.sh || handle_error "Failed to run $loc/ubuntu/kernel/dependencies.sh"
+
+if [[ -d $KERNEL_DIR ]]; then
+	cd "$KERNEL_DIR"
+        log "Directory $KERNEL_DIR exists"
+        $loc/config.sh $KERNEL_DIR base
+else
+        handle_error "Failed to change to kernel directory, Directory $KERNEL_DIR doesn't exists"
+fi
+
