b4cb0a23092df61c6accc60baed8fc9750aa58fe
PKAditya <pkngssaditya99@gmail.com>
2024-11-28 17:49:16 +0800

    Integrated the build(installation) part of the kernel to main running of the script
    Signed-off-by: PKAditya <pkngssaditya99@gmail.com>

--- ubuntu/kernel/run.sh
+++ ubuntu/kernel/run.sh
@@ -16,6 +16,7 @@ log() {
 # function defined to handle error
 handle_error() {
 	log "ERROR: $1" >> $log
+	echo "Refer $log to check the details of the error"
 	exit 1
 }
 
@@ -28,7 +29,12 @@ $loc/ubuntu/kernel/dependencies.sh || handle_error "Failed to run $loc/ubuntu/ke
 if [[ -d $KERNEL_DIR ]]; then
 	cd "$KERNEL_DIR"
         log "Directory $KERNEL_DIR exists"
-        $loc/ubuntu/kernel/config.sh $KERNEL_DIR _auto_base_
+	log "Running the configuration script: $loc/ubuntu/kernel/config.sh"
+        $loc/ubuntu/kernel/config.sh $KERNEL_DIR _auto_base_ || handle_error "Failed running $loc/ubuntu/kernel/config.sh"
+	log "Successfully ran the configuration"
+
+	log "Proceeding with building the kernel configured with the local version"
+	$loc/ubuntu/kernel/install.sh || handle_error "Cannot build the configured kernel."
 else
         handle_error "Failed to change to kernel directory, Directory $KERNEL_DIR doesn't exists"
 fi
