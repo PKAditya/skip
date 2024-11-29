c97e5a8f5997d871768a208b6439caad99939dd1
PKAditya <pkngssaditya99@gmail.com>
2024-11-29 14:49:58 +0800

    Changed configuration steps for the kernel.
    Signed-off-by: PKAditya <pkngssaditya99@gmail.com>

--- centos/kernel/config.sh
+++ centos/kernel/config.sh
@@ -32,7 +32,7 @@ log "Navigated to the kernel directory"
 log "Configuring the kernel..."
 
 # Create a configuration file to build the kernel
-make defconfig || handle_error "Failed to create config file"
+make olddefconfig || handle_error "Failed to create config file"
 
 # Change the local version to our own version
 sed -i 's/^CONFIG_LOCALVERSION=.*$/CONFIG_LOCALVERSION="'$name'"/' .config
@@ -49,6 +49,6 @@ scripts/config --disable  CONFIG_DEBUG_INFO_BTF
 scripts/config --disable NET_VENDOR_NETRONOME 
 
 # Regenerate the old config file so the changes made will take effect
-make olddefconfig || handle_error "Failed to update the config file"
+#make olddefconfig || handle_error "Failed to update the config file"
 
 log "Kernel configured successfully."
