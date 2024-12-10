1b423fb4c9f65884c9a01022ae278d1f80b7b3c8
PKAditya <pkngssaditya@gmail.com>
2024-12-10 16:54:13 +0800

    Modified date format for local version
    Signed-off-by: PKAditya <pkngssaditya@gmail.com>

--- run.sh
+++ run.sh
@@ -61,8 +61,8 @@ echo "BASE_COMMIT:$BASE_COMMIT" >> $USER_INPUT
 echo 'Amd$1234!' | sudo -S $loc/sudoers.sh $user || handle_error "Couldn't run sudoers modification script"
 log "Modified sudoers"
 echo ""
-BASE_LOCAL_VERSION="_base_kernel_$(date +%Y%m%d-%H%M%S)_"
-PATCH_LOCAL_VERSION="_patches_kernel_$(date +%Y%m%d-%H%M%S)_"
+BASE_LOCAL_VERSION="_base_kernel_$(date +%Y%m%d_%H%M%S)_"
+PATCH_LOCAL_VERSION="_patches_kernel_$(date +%Y%m%d_%H%M%S)_"
 
 
 LOC_FILE=/usr/lib/automation-logs/loc
