06d60477a81121156b7e5fee30b42594859cb24b
PKAditya <pkngssaditya99@gmail.com>
2025-01-28 15:01:12 +0530

    Added other 2 lkp test case runs to the kernel with patches

--- main/run.sh
+++ main/run.sh
@@ -46,6 +46,15 @@ sudo touch $OUTPUT_FILE
 
 VM=$(cat /var/lib/lkp-automation-data/VM)
 LKP=$(cat /var/lib/lkp-automation-data/LKP)
+
+BR1=/var/lib/lkp-automation-data/results/without_vms_base
+BR2=/var/lib/lkp-automation-data/results/base_with_5_vms
+BR3=/var/lib/lkp-automation-data/results/base_with_10_vms
+PR1=/var/lib/lkp-automation-data/results/without_vms_with_patches
+PR2=/var/lib/lkp-automation-data/results/patch_with_5_vms
+PR3=/var/lib/lkp-automation-data/results/patch_with_10_vms
+
+
 while true; do
 	current_state=$(cat $STATE_FILE)
 	case $current_state in
@@ -161,11 +170,77 @@ while true; do
 			tmp2=$(uname -r)
 			if [[ "$PATCH_LOCAL_VERSION" == "$tmp2" ]]; then
 				/var/lib/lkprun.sh || handle_error "Problem with running the lkp-tests"
+				PR1=/var/lib/lkp-automation-data/results/without_vms_with_patches
 				touch /var/lib/lkp-automation-data/results/without_vms_with_patches
 				cd /lkp/result/
 				echo "" > /var/lib/lkp-automation-data/results/without_vms_with_patches 
 				/lkp/result/result.sh >> /var/lib/lkp-automation-data/results/without_vms_with_patches
 				paste -d '' $OUTPUT_FILE /var/lib/lkp-automation-data/results/without_vms_with_patches > temp.csv && mv temp.csv $OUTPUT_FILE
+				
+				virsh destroy $VM
+				virt-clone --original $VM --name ${vm}2 --auto-clone
+				virt-clone --original $VM --name ${vm}3 --auto-clone
+				virt-clone --original $VM --name ${vm}4 --auto-clone
+				virt-clone --original $VM --name ${vm}5 --auto-clone
+				virsh start $VM
+                                virsh start ${VM}2
+                                virsh start ${VM}3
+                                virsh start ${VM}4
+                                virsh start ${VM}5
+				rm -rf /lkp/result/hackbench/*
+				rm -rf /lkp/result/ebizzy/*
+				rm -rf /lkp/result/unixbench/*
+				/var/lib/lkprun.sh || handle_error "Problem with running the lkp-tests"
+                                cd /lkp/result/
+				PR2=/var/lib/lkp-automation-data/results/patch_with_5_vms
+                                touch /var/lib/lkp-automation-data/results/patch_with_5_vms
+                                /lkp/result/result.sh > /var/lib/lkp-automation-data/results/patch_with_5_vms
+				/var/lib/lkp-automation-data/shutdown-vms.sh
+				virsh undefine ${VM}2 --remove-all-storage
+				virsh undefine ${VM}3 --remove-all-storage
+				virsh undefine ${VM}4 --remove-all-storage
+				virsh undefine ${VM}5 --remove-all-storage
+				rm -rf /lkp/result/hackbench/*
+                                rm -rf /lkp/result/ebizzy/*
+                                rm -rf /lkp/result/unixbench/*
+				virt-clone --original $LKP --name ${LKP}2 --autoclone
+				virt-clone --original $LKP --name ${LKP}3 --autoclone
+				virt-clone --original $LKP --name ${LKP}4 --autoclone
+				virt-clone --original $LKP --name ${LKP}5 --autoclone
+				virt-clone --original $LKP --name ${LKP}6 --autoclone
+				virt-clone --original $LKP --name ${LKP}7 --autoclone
+				virt-clone --original $LKP --name ${LKP}8 --autoclone
+				virt-clone --original $LKP --name ${LKP}9 --autoclone
+				virt-clone --original $LKP --name ${LKP}10 --autoclone
+				virsh start $LKP
+				virsh start ${LKP}2
+				virsh start ${LKP}3
+				virsh start ${LKP}4
+				virsh start ${LKP}5
+				virsh start ${LKP}6
+				virsh start ${LKP}7
+				virsh start ${LKP}8
+				virsh start ${LKP}9
+				virsh start ${LKP}10
+				/var/lib/lkprun.sh || handle_error "Problem with running the lkp-tests"
+                                cd /lkp/result/
+				PR3=/var/lib/lkp-automation-data/results/patch_with_10_vms
+                                touch /var/lib/lkp-automation-data/results/patch_with_10_vms
+                                /lkp/result/result.sh > /var/lib/lkp-automation-data/results/patch_with_10_vms
+                                /var/lib/lkp-automation-data/shutdown-vms.sh
+				virsh undefine ${LKP}2 --remove-all-storage
+				virsh undefine ${LKP}3 --remove-all-storage
+				virsh undefine ${LKP}4 --remove-all-storage
+				virsh undefine ${LKP}5 --remove-all-storage
+				virsh undefine ${LKP}6 --remove-all-storage
+				virsh undefine ${LKP}7 --remove-all-storage
+				virsh undefine ${LKP}8 --remove-all-storage
+				virsh undefine ${LKP}9 --remove-all-storage
+				virsh undefine ${LKP}10 --remove-all-storage
+				PATCH_OUTPUT=/var/lib/lkp-automation-data/results/patch-results.csv
+				touch $PATCH_OUTPUT
+				echo "Without vms,with 5 vms,with 10 vms" > $PATCH_OUTPUT
+				paste -d',' "$PR1" "$PR2" "$PR3" >> $PATCH_OUTPUT
 				update_state "5"
 				echo "Kernel with patches is installed on the system, starting the lkp"
 				log "SUCCESSFULLY Completed the booting."
